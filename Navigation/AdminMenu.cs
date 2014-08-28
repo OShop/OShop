cf5028119717434ff1fb23a570f9217c68afc030
Codinlab <thierry@codinlab.fr>
2014-08-28 19:21:41 +0200

    Add AdminMenu, icon and base permissions

---
+++ Navigation/AdminMenu.cs
@@ -0,0 +1,26 @@
+﻿using Orchard.Localization;
+using Orchard.UI.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OShop.Permissions;
+
+namespace OShop.Navigation {
+    public class AdminMenu : INavigationProvider {
+        public Localizer T { get; set; }
+
+        public AdminMenu() {
+            T = NullLocalizer.Instance;
+        }
+
+        public string MenuName { get { return "admin"; } }
+
+        public void GetNavigation(NavigationBuilder builder) {
+            builder.AddImageSet("oshop")
+                .Add(T("OShop"), "6",
+                    menu => menu.Add(T("OShop"), "0", item => item.Action("Index", "Admin", new { area = "Orchard.MediaLibrary" })
+                        .Permission(OShopPermissions.AccessShopPanel)));
+        }
+    }
+}
---
+++ Permissions/OShopPermissions.cs
@@ -0,0 +1,44 @@
+﻿using Orchard.Environment.Extensions.Models;
+using Orchard.Security.Permissions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OShop.Permissions {
+    public class OShopPermissions : IPermissionProvider {
+        public static readonly Permission ManageShopSettings = new Permission { Description = "Manage Shop Settings", Name = "ManageShopSettings" };
+
+        public static readonly Permission AccessShopPanel = new Permission {
+            Description = "Access Shop Panel",
+            Name = "AccessShopPanel",
+            ImpliedBy = new[] {
+            OShopPermissions.ManageShopSettings
+        }
+        };
+
+        public Feature Feature { get; set; }
+
+        public IEnumerable<Permission> GetPermissions() {
+            return new[] {
+                OShopPermissions.AccessShopPanel,
+                OShopPermissions.ManageShopSettings
+            };
+        }
+
+        public IEnumerable<PermissionStereotype> GetDefaultStereotypes() {
+            return new[] {
+                new PermissionStereotype {
+                    Name = "Administrator",
+                    Permissions = new[] {
+                        OShopPermissions.ManageShopSettings
+                    }
+                },
+                new PermissionStereotype {
+                    Name = "Editor",
+                    //Permissions = new[] {}
+                }
+            };
+        }
+    }
+}
