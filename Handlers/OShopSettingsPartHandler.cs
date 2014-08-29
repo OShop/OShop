176d7ee7e3cb626e1d005a1ad0d35a6a0316d39b
Codinlab <thierry@codinlab.fr>
2014-08-29 10:41:53 +0200

    Create OShopSettingsPart

---
+++ Handlers/OShopSettingsPartHandler.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+using Orchard.ContentManagement.Handlers;
+using OShop.Models;
+
+namespace OShop.Handlers {
+    [UsedImplicitly]
+    public class OShopSettingsPartHandler : ContentHandler {
+        public OShopSettingsPartHandler() {
+            Filters.Add(new ActivatingFilter<OShopSettingsPart>("Site"));
+        }
+    }
+}
---
+++ Models/OShopSettingsPart.cs
@@ -0,0 +1,7 @@
+﻿using Orchard.ContentManagement;
+
+namespace OShop.Models {
+    public class OShopSettingsPart : ContentPart {
+
+    }
+}
