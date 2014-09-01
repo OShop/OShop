2ca7646b33958943a74a87ce816492977f9bba9e
Codinlab <thierry@codinlab.fr>
2014-09-01 14:43:50 +0200

    Add Products list View

--- Controllers/ProductsAdminController.cs
+++ Controllers/ProductsAdminController.cs
@@ -1,21 +1,193 @@
-﻿using Orchard.Environment.Extensions;
+﻿using Orchard;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Models;
+using Orchard.Core.Common.Models;
+using Orchard.Core.Contents.ViewModels;
+using Orchard.Data;
+using Orchard.DisplayManagement;
+using Orchard.Environment.Extensions;
+using Orchard.Localization;
+using Orchard.Mvc;
+using Orchard.Mvc.Extensions;
+using Orchard.Settings;
 using Orchard.UI.Admin;
+using Orchard.UI.Navigation;
+using Orchard.UI.Notify;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace OShop.Controllers
 {
     [Admin]
+    [ValidateInput(false)]
     [OrchardFeature("OShop.Products")]
     public class ProductsAdminController : Controller
     {
-        // GET: ProductsAdmin
-        public ActionResult List()
-        {
-            return View();
+        private readonly IContentManager _contentManager;
+        private readonly IContentDefinitionManager _contentDefinitionManager;
+        private readonly ITransactionManager _transactionManager;
+        private readonly ISiteService _siteService;
+
+        public ProductsAdminController(
+            IOrchardServices orchardServices,
+            IContentManager contentManager,
+            IContentDefinitionManager contentDefinitionManager,
+            ITransactionManager transactionManager,
+            ISiteService siteService,
+            IShapeFactory shapeFactory) {
+            Services = orchardServices;
+            _contentManager = contentManager;
+            _contentDefinitionManager = contentDefinitionManager;
+            _transactionManager = transactionManager;
+            _siteService = siteService;
+
+            Shape = shapeFactory;
+            T = NullLocalizer.Instance;
         }
+
+        dynamic Shape { get; set; }
+        public IOrchardServices Services { get; private set; }
+        public Localizer T { get; set; }
+
+        public ActionResult List(ListContentsViewModel model, PagerParameters pagerParameters)
+        {
+            var pager = new Pager(_siteService.GetSiteSettings(), pagerParameters);
+
+            var versionOptions = VersionOptions.Latest;
+            switch (model.Options.ContentsStatus) {
+                case ContentsStatus.Published:
+                    versionOptions = VersionOptions.Published;
+                    break;
+                case ContentsStatus.Draft:
+                    versionOptions = VersionOptions.Draft;
+                    break;
+                case ContentsStatus.AllVersions:
+                    versionOptions = VersionOptions.AllVersions;
+                    break;
+                default:
+                    versionOptions = VersionOptions.Latest;
+                    break;
+            }
+
+            var query = _contentManager.Query(versionOptions, GetProductTypes().Select(ctd => ctd.Name).ToArray());
+
+            if (!string.IsNullOrEmpty(model.TypeName)) {
+                var contentTypeDefinition = _contentDefinitionManager.GetTypeDefinition(model.TypeName);
+                if (contentTypeDefinition == null)
+                    return HttpNotFound();
+
+                model.TypeDisplayName = !string.IsNullOrWhiteSpace(contentTypeDefinition.DisplayName)
+                                            ? contentTypeDefinition.DisplayName
+                                            : contentTypeDefinition.Name;
+                query = query.ForType(model.TypeName);
+            }
+
+            switch (model.Options.OrderBy) {
+                case ContentsOrder.Modified:
+                    query = query.OrderByDescending<CommonPartRecord>(cr => cr.ModifiedUtc);
+                    break;
+                case ContentsOrder.Published:
+                    query = query.OrderByDescending<CommonPartRecord>(cr => cr.PublishedUtc);
+                    break;
+                case ContentsOrder.Created:
+                    query = query.OrderByDescending<CommonPartRecord>(cr => cr.CreatedUtc);
+                    break;
+            }
+
+            model.Options.SelectedFilter = model.TypeName;
+            model.Options.FilterOptions = GetProductTypes()
+                .Select(ctd => new KeyValuePair<string, string>(ctd.Name, ctd.DisplayName))
+                .ToList().OrderBy(kvp => kvp.Value);
+
+            var pagerShape = Shape.Pager(pager).TotalItemCount(query.Count());
+            var pageOfContentItems = query.Slice(pager.GetStartIndex(), pager.PageSize).ToList();
+
+            var list = Shape.List();
+            list.AddRange(pageOfContentItems.Select(ci => _contentManager.BuildDisplay(ci, "ProductsSummaryAdmin")));
+
+            var viewModel = Shape.ViewModel()
+                .ContentItems(list)
+                .Pager(pagerShape)
+                .Options(model.Options)
+                .TypeDisplayName(model.TypeDisplayName ?? "");
+
+            return View(viewModel);
+        }
+
+        [HttpPost, ActionName("List")]
+        [FormValueRequired("submit.Filter")]
+        public ActionResult ListFilterPOST(ContentOptions options) {
+            var routeValues = ControllerContext.RouteData.Values;
+            if (options != null) {
+                routeValues["Options.OrderBy"] = options.OrderBy; //todo: don't hard-code the key
+                routeValues["Options.ContentsStatus"] = options.ContentsStatus; //todo: don't hard-code the key
+                if (GetProductTypes().Any(ctd => string.Equals(ctd.Name, options.SelectedFilter, StringComparison.OrdinalIgnoreCase))) {
+                    routeValues["id"] = options.SelectedFilter;
+                }
+                else {
+                    routeValues.Remove("id");
+                }
+            }
+
+            return RedirectToAction("List", routeValues);
+        }
+
+        [HttpPost, ActionName("List")]
+        [FormValueRequired("submit.BulkEdit")]
+        public ActionResult ListPOST(ContentOptions options, IEnumerable<int> itemIds, string returnUrl) {
+            if (itemIds != null) {
+                var checkedContentItems = _contentManager.GetMany<ContentItem>(itemIds, VersionOptions.Latest, QueryHints.Empty);
+                switch (options.BulkAction) {
+                    case ContentsBulkAction.None:
+                        break;
+                    case ContentsBulkAction.PublishNow:
+                        foreach (var item in checkedContentItems) {
+                            if (!Services.Authorizer.Authorize(Orchard.Core.Contents.Permissions.PublishContent, item, T("Couldn't publish selected content."))) {
+                                _transactionManager.Cancel();
+                                return new HttpUnauthorizedResult();
+                            }
+
+                            _contentManager.Publish(item);
+                        }
+                        Services.Notifier.Information(T("Content successfully published."));
+                        break;
+                    case ContentsBulkAction.Unpublish:
+                        foreach (var item in checkedContentItems) {
+                            if (!Services.Authorizer.Authorize(Orchard.Core.Contents.Permissions.PublishContent, item, T("Couldn't unpublish selected content."))) {
+                                _transactionManager.Cancel();
+                                return new HttpUnauthorizedResult();
+                            }
+
+                            _contentManager.Unpublish(item);
+                        }
+                        Services.Notifier.Information(T("Content successfully unpublished."));
+                        break;
+                    case ContentsBulkAction.Remove:
+                        foreach (var item in checkedContentItems) {
+                            if (!Services.Authorizer.Authorize(Orchard.Core.Contents.Permissions.DeleteContent, item, T("Couldn't remove selected content."))) {
+                                _transactionManager.Cancel();
+                                return new HttpUnauthorizedResult();
+                            }
+
+                            _contentManager.Remove(item);
+                        }
+                        Services.Notifier.Information(T("Content successfully removed."));
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            return this.RedirectLocal(returnUrl, () => RedirectToAction("List"));
+        }
+
+        private IEnumerable<ContentTypeDefinition> GetProductTypes() {
+            return _contentDefinitionManager.ListTypeDefinitions().Where(ctd =>
+                ctd.Parts.Any(p => p.PartDefinition.Name == "ProductPart"));
+        }
+
     }
 }
