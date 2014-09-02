a12ecc127f758b1a0d6af1662653ca13340d5620
Codinlab <thierry@codinlab.fr>
2014-09-02 12:59:43 +0200

    Update Vat DisplayName helper

--- Helpers/VatExtensions.cs
+++ Helpers/VatExtensions.cs
@@ -1,11 +1,12 @@
 ﻿using OShop.Models;
 using System.Globalization;
 using System;
+using Orchard.Localization;
 
 namespace OShop.Helpers {
     public static class VatExtensions {
-        public static String DisplayName(this VatRecord record, IFormatProvider provider) {
-            return String.Format(provider, "{0} ({1:P})", record.Name, record.Rate);
+        public static LocalizedString DisplayName(this VatRecord record, Localizer T) {
+            return T("{0} ({1:P})", record.Name, record.Rate);
         }
 
         public static decimal GetVat(this VatRecord record, decimal price) {
