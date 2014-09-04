519fe47432cb03c40f42da12ed9ec9b84e0de8f0
Codinlab <thierry@codinlab.fr>
2014-09-04 18:48:34 +0200

    Update ShoppingCartRecord

--- Models/ShoppingCartRecord.cs
+++ Models/ShoppingCartRecord.cs
@@ -8,11 +8,11 @@ namespace OShop.Models {
         public virtual int Id { get; set; }
         public virtual Guid Guid { get; set; }
         public virtual DateTime ModifiedUtc { get; set; }
-        public virtual int OwnerId { get; set; }
-        public virtual IList<ShoppingCartItem> Items { get; set; }
+        public virtual int? OwnerId { get; set; }
+        public virtual IList<ShoppingCartItemRecord> Items { get; set; }
 
         internal ShoppingCartRecord() {
-            Items = new List<ShoppingCartItem>();
+            Items = new List<ShoppingCartItemRecord>();
         }
     }
 }
