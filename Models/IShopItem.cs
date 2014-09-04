b2167d37bad96d96d1ee9b74a567be9ab3533330
Codinlab <thierry@codinlab.fr>
2014-09-04 10:17:27 +0200

    Create ShopItem and ShopItemProvider interfaces

---
+++ Models/IShopItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OShop.Models {
+    public interface IShopItem {
+        int Id { get; }
+        string ItemType { get; }
+        string SKU { get; }
+        string Designation { get; }
+        string Description { get; }
+        decimal GetUnitPrice(int Quantity = 1);
+        VatRecord VAT { get; }
+    }
+}
---
+++ Services/IShopItemProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OShop.Services {
+    public interface IShopItemProvider {
+        Int16 Priority { get; }
+    }
+}
