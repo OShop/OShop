0635f5c87760d7bda88c1cbdac67f1923617b696
Codinlab <thierry@codinlab.fr>
2014-09-17 11:08:38 +0200

    Add ShoppingCartUpdateViewModel

---
+++ ViewModels/ShoppingCartUpdateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OShop.ViewModels {
+    public class ShoppingCartUpdateViewModel {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public bool IsRemoved { get; set; }
+    }
+}
