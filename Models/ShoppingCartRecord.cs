934b3b6674433d51b729ae55ec7c01fe1ca28b4d
Codinlab <thierry@codinlab.fr>
2014-09-18 16:34:57 +0200

    Clean ShoppingCartRecord usings

--- Models/ShoppingCartRecord.cs
+++ Models/ShoppingCartRecord.cs
@@ -1,8 +1,6 @@
 ﻿using Orchard.Data.Conventions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace OShop.Models {
     public class ShoppingCartRecord {
