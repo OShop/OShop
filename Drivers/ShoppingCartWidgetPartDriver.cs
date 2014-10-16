3b4f13d802a51633e79c821409127edee9bf6f65
Codinlab <thierry@codinlab.fr>
2014-10-16 16:19:55 +0200

    Clean ShoppingCartWidgetPartDriver usings

--- Drivers/ShoppingCartWidgetPartDriver.cs
+++ Drivers/ShoppingCartWidgetPartDriver.cs
@@ -1,13 +1,6 @@
-﻿using Orchard;
-using Orchard.ContentManagement.Drivers;
+﻿using Orchard.ContentManagement.Drivers;
 using Orchard.Environment.Extensions;
-using Orchard.Environment.Features;
 using OShop.Models;
-using OShop.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace OShop.Drivers {
     [OrchardFeature("OShop.ShoppingCart")]
