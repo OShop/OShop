0fe996099fc8b8989203d05436d832e7548d02df
Codinlab <thierry@codinlab.fr>
2014-10-21 17:21:51 +0200

    Add field settings to LocationFieldViewModel

--- ViewModels/LocationFieldViewModel.cs
+++ ViewModels/LocationFieldViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using OShop.Models;
+using OShop.Settings;
 using System.Collections.Generic;
 
 namespace OShop.ViewModels {
     public class LocationFieldViewModel {
+        public LocationFieldSettings Settings { get; set; }
         public IEnumerable<LocationsCountryRecord> Countries { get; set; }
         public IEnumerable<LocationsStateRecord> States { get; set; }
         public int CountryId { get; set; }
