dd4c29d8e46ddad6180013bc401df246564a2d44
Codinlab <thierry@codinlab.fr>
2014-10-22 15:13:45 +0200

    Fix CustomerAddressPart

--- Models/CustomerAddressPart.cs
+++ Models/CustomerAddressPart.cs
@@ -8,32 +8,32 @@ namespace OShop.Models {
             set { this.Store(x => x.AddressAlias, value); }
         }
         public String Company {
-            get { return this.Retrieve(x => x.AddressAlias); }
-            set { this.Store(x => x.AddressAlias, value); }
+            get { return this.Retrieve(x => x.Company); }
+            set { this.Store(x => x.Company, value); }
         }
         public String FirstName {
-            get { return this.Retrieve(x => x.AddressAlias); }
-            set { this.Store(x => x.AddressAlias, value); }
+            get { return this.Retrieve(x => x.FirstName); }
+            set { this.Store(x => x.FirstName, value); }
         }
         public String LastName {
-            get { return this.Retrieve(x => x.AddressAlias); }
-            set { this.Store(x => x.AddressAlias, value); }
+            get { return this.Retrieve(x => x.LastName); }
+            set { this.Store(x => x.LastName, value); }
         }
         public String Address1 {
-            get { return this.Retrieve(x => x.AddressAlias); }
-            set { this.Store(x => x.AddressAlias, value); }
+            get { return this.Retrieve(x => x.Address1); }
+            set { this.Store(x => x.Address1, value); }
         }
         public String Address2 {
-            get { return this.Retrieve(x => x.AddressAlias); }
-            set { this.Store(x => x.AddressAlias, value); }
+            get { return this.Retrieve(x => x.Address2); }
+            set { this.Store(x => x.Address2, value); }
         }
         public String Zipcode {
-            get { return this.Retrieve(x => x.AddressAlias); }
-            set { this.Store(x => x.AddressAlias, value); }
+            get { return this.Retrieve(x => x.Zipcode); }
+            set { this.Store(x => x.Zipcode, value); }
         }
         public String City {
-            get { return this.Retrieve(x => x.AddressAlias); }
-            set { this.Store(x => x.AddressAlias, value); }
+            get { return this.Retrieve(x => x.City); }
+            set { this.Store(x => x.City, value); }
         }
         public LocationsCountryRecord Country {
             get { return Record.LocationsCountryRecord; }
