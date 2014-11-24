2fa46ff21c5edb532e353f99eda8d3a231724fb9
Codinlab <thierry@codinlab.fr>
2014-11-24 16:30:42 +0100

    Customers : Add CustomerAddressResolver

---
+++ Services/ShoppingCartResolvers/CustomerAddressResolver.cs
@@ -0,0 +1,41 @@
+﻿using Orchard.Environment.Extensions;
+using OShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OShop.Services.ShoppingCartResolvers {
+    [OrchardFeature("OShop.Customers")]
+    public class CustomerAddressResolver : IShoppingCartResolver {
+        private readonly ICustomersService _customersService;
+
+        public CustomerAddressResolver (
+            ICustomersService customersService) {
+            _customersService = customersService;
+        }
+
+        public int Priority {
+            get { return 950; }
+        }
+
+        public void ResolveCart(IShoppingCartService ShoppingCartService, ref ShoppingCart Cart) {
+            Int32 billingAddressId = ShoppingCartService.GetProperty<int>("BillingAddressId");
+            Int32 shippingAddressId = ShoppingCartService.GetProperty<int>("ShippingAddressId");
+
+            if (billingAddressId > 0) {
+                var billingAddress = _customersService.GetAddresses().Where(a => a.Id == billingAddressId).FirstOrDefault();
+                if (billingAddress != null) {
+                    Cart.BillingAddress = billingAddress;
+                }
+            }
+
+            if (shippingAddressId > 0) {
+                var shippingAddress = _customersService.GetAddresses().Where(a => a.Id == shippingAddressId).FirstOrDefault();
+                if (shippingAddress != null) {
+                    Cart.ShippingAddress = shippingAddress;
+                }
+            }
+        }
+    }
+}
