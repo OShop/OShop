0845a96d40f0362281e1894bb3373068b1cfdf75
Codinlab <thierry@codinlab.fr>
2014-11-24 16:29:51 +0100

    Move LocationResolver logic to ShippingZoneResolver.
    Stop providing an dummy address to ShoppingCart

--- Services/ShoppingCartResolvers/ShippingZoneResolver.cs
+++ Services/ShoppingCartResolvers/ShippingZoneResolver.cs
@@ -6,30 +6,41 @@ using System;
 namespace OShop.Services.ShoppingCartResolvers {
     [OrchardFeature("OShop.Shipping")]
     public class ShippingZoneResolver : IShoppingCartResolver {
-        private readonly ILocationsService _locationService;
+        private readonly ILocationsService _locationsService;
 
         public ShippingZoneResolver(
-            ILocationsService locationService) {
-            _locationService = locationService;
+            ILocationsService locationsService) {
+            _locationsService = locationsService;
         }
 
         public Int32 Priority {
-            get { return 990; }
+            get { return 900; }
         }
 
         public void ResolveCart(IShoppingCartService ShoppingCartService, ref ShoppingCart Cart) {
+            LocationsStateRecord state = null;
+            LocationsCountryRecord country = null;
+
             if (Cart.ShippingAddress != null) {
-                var state = _locationService.GetState(Cart.ShippingAddress.StateId);
-                var country = _locationService.GetCountry(Cart.ShippingAddress.CountryId);
-                if (state != null && state.Enabled && state.ShippingZoneRecord != null) {
-                    Cart.ShippingZone = state.ShippingZoneRecord;
+                state = _locationsService.GetState(Cart.ShippingAddress.StateId);
+                country = _locationsService.GetCountry(Cart.ShippingAddress.CountryId);
+            }
+            else {
+                Int32 countryId = ShoppingCartService.GetProperty<int>("CountryId");
+                if (countryId > 0) {
+                    country = _locationsService.GetCountry(countryId);
+                    Int32 stateId = ShoppingCartService.GetProperty<int>("StateId");
+                    if (stateId > 0) {
+                        state = _locationsService.GetState(stateId);
+                    }
                 }
-                else if (country != null && country.Enabled && country.ShippingZoneRecord != null) {
-                    Cart.ShippingZone = country.ShippingZoneRecord;
-                }
-                else {
-                    Cart.ShippingZone = null;
-                }
+            }
+
+            if (state != null && state.Enabled && state.ShippingZoneRecord != null) {
+                Cart.ShippingZone = state.ShippingZoneRecord;
+            }
+            else if (country != null && country.Enabled && country.ShippingZoneRecord != null) {
+                Cart.ShippingZone = country.ShippingZoneRecord;
             }
             else {
                 Cart.ShippingZone = null;
