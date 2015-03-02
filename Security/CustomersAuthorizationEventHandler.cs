ff73c44fa3fea0534f8bed4f903966e19de3106e
Codinlab <thierry@codinlab.fr>
2015-03-02 18:08:28 +0100

    Customers : Rewrite CustomersAuthorizationEventHandler

--- Security/CustomersAuthorizationEventHandler.cs
+++ Security/CustomersAuthorizationEventHandler.cs
@@ -4,59 +4,63 @@ using Orchard.Environment.Extensions;
 using Orchard.Security;
 using OShop.Models;
 using OShop.Permissions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace OShop.Security {
     [OrchardFeature("OShop.Customers")]
     public class CustomersAuthorizationEventHandler : IAuthorizationServiceEventHandler {
-        public void Checking(CheckAccessContext context) {
-            // Override permissions for Customer account infos
-            if (context.Content.Is<CustomerPart>() || context.Content.Is<CustomerAddressPart>()) {
+        public void Checking(CheckAccessContext context) { }
+
+        public void Adjust(CheckAccessContext context) {
+            if (!context.Granted &&
+                context.Content.Is<ICommonPart>() &&
+                (context.Content.Is<CustomerPart>() || context.Content.Is<CustomerAddressPart>())) {
+
                 if (context.Permission.Name == Orchard.Core.Contents.Permissions.PublishContent.Name) {
-                    context.Permission = HasOwnership(context) ? CustomersPermissions.EditOwnCustomerAccount : CustomersPermissions.ManageCustomerAccounts;
+                    context.Adjusted = true;
+                    context.Permission = HasOwnership(context.User, context.Content) ? CustomersPermissions.EditOwnCustomerAccount : CustomersPermissions.ManageCustomerAccounts;
                 }
                 else if (context.Permission.Name == Orchard.Core.Contents.Permissions.PublishOwnContent.Name) {
+                    context.Adjusted = true;
                     context.Permission = CustomersPermissions.EditOwnCustomerAccount;
                 }
                 else if (context.Permission.Name == Orchard.Core.Contents.Permissions.EditContent.Name) {
-                    context.Permission = HasOwnership(context) ? CustomersPermissions.EditOwnCustomerAccount : CustomersPermissions.ManageCustomerAccounts;
+                    context.Adjusted = true;
+                    context.Permission = HasOwnership(context.User, context.Content) ? CustomersPermissions.EditOwnCustomerAccount : CustomersPermissions.ManageCustomerAccounts;
                 }
                 else if (context.Permission.Name == Orchard.Core.Contents.Permissions.EditOwnContent.Name) {
+                    context.Adjusted = true;
                     context.Permission = CustomersPermissions.EditOwnCustomerAccount;
                 }
                 else if (context.Permission.Name == Orchard.Core.Contents.Permissions.DeleteContent.Name) {
-                    context.Permission = HasOwnership(context) ? CustomersPermissions.EditOwnCustomerAccount : CustomersPermissions.ManageCustomerAccounts;
+                    context.Adjusted = true;
+                    context.Permission = HasOwnership(context.User, context.Content) ? CustomersPermissions.EditOwnCustomerAccount : CustomersPermissions.ManageCustomerAccounts;
                 }
                 else if (context.Permission.Name == Orchard.Core.Contents.Permissions.DeleteOwnContent.Name) {
+                    context.Adjusted = true;
                     context.Permission = CustomersPermissions.EditOwnCustomerAccount;
                 }
                 else if (context.Permission.Name == Orchard.Core.Contents.Permissions.ViewContent.Name) {
-                    context.Permission = HasOwnership(context) ? CustomersPermissions.ViewOwnCustomerAccount : CustomersPermissions.ViewCustomerAccounts;
+                    context.Adjusted = true;
+                    context.Permission = HasOwnership(context.User, context.Content) ? CustomersPermissions.ViewOwnCustomerAccount : CustomersPermissions.ViewCustomerAccounts;
                 }
                 else if (context.Permission.Name == Orchard.Core.Contents.Permissions.ViewOwnContent.Name) {
+                    context.Adjusted = true;
                     context.Permission = CustomersPermissions.ViewOwnCustomerAccount;
                 }
             }
         }
 
-        public void Adjust(CheckAccessContext context) {
-        }
-
         public void Complete(CheckAccessContext context) { }
 
-        private static bool HasOwnership(CheckAccessContext context) {
-            if (context.User == null || context.Content == null)
+        private static bool HasOwnership(IUser user, IContent content) {
+            if (user == null || content == null)
                 return false;
 
-            var common = context.Content.As<ICommonPart>();
+            var common = content.As<ICommonPart>();
             if (common == null || common.Owner == null)
                 return false;
 
-            return context.User.Id == common.Owner.Id;
+            return user.Id == common.Owner.Id;
         }
-
     }
 }
