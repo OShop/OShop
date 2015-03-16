cbdd5f57df521240e01704c1dc9825c25cea441b
Codinlab <thierry@codinlab.fr>
2015-03-16 11:32:55 +0100

    Orders : Add CustomerOrderPartDriver

---
+++ Drivers/CustomerOrderPartDriver.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.ContentManagement.Drivers;
+using Orchard.Environment.Extensions;
+using OShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OShop.Drivers {
+    [OrchardFeature("OShop.Customers")]
+    public class CustomerOrderPartDriver : ContentPartDriver<CustomerOrderPart> {
+
+    }
+}
