a06f6e036c8905a7a7cdaf236d07c2a3e7a945ad
Codinlab <thierry@codinlab.fr>
2015-03-17 14:43:17 +0100

    Customers : Move OrdersCustomerPartDriver to Customers feature

--- Drivers/OrdersCustomerPartDriver.cs
+++ Drivers/OrdersCustomerPartDriver.cs
@@ -14,7 +14,7 @@ using System.Linq;
 using System.Web;
 
 namespace OShop.Drivers {
-    [OrchardFeature("OShop.Orders")]
+    [OrchardFeature("OShop.Customers")]
     public class OrdersCustomerPartDriver : ContentPartDriver<CustomerPart> {
         private readonly IContentManager _contentManager;
         private readonly ICustomersService _customersService;
