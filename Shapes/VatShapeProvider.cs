7f05cb44709c05873c2fcf59c0fdae0b4450bc66
Codinlab <thierry@codinlab.fr>
2015-03-25 16:31:39 +0100

    VAT : Add ShoppingCart shape alternates

--- Shapes/VatShapeProvider.cs
+++ Shapes/VatShapeProvider.cs
@@ -10,6 +10,14 @@ namespace OShop.Shapes {
                     displaying.ShapeMetadata.Alternates.Add("Price__Vat");
                     displaying.ShapeMetadata.Alternates.Add("Price_" + displaying.ShapeMetadata.DisplayType + "__Vat");
                 });
+            builder.Describe("ShoppingCart_CartItems")
+                .OnDisplaying(displaying => {
+                    displaying.ShapeMetadata.Alternates.Add("ShoppingCart_CartItems__Vat");
+                });
+            builder.Describe("ShoppingCart_Widget")
+                .OnDisplaying(displaying => {
+                    displaying.ShapeMetadata.Alternates.Add("ShoppingCart_Widget__Vat");
+                });
         }
     }
 }
