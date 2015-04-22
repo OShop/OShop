8d2fca5883415475045441fdf5acf2a7c2878c04
Codinlab <thierry@codinlab.fr>
2015-04-22 14:33:28 +0200

    Add "CurrentNumberFormat" to WorkContext

---
+++ Services/CurrencyWorkContext.cs
@@ -0,0 +1,22 @@
+﻿using Orchard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OShop.Services {
+    public class CurrencyWorkContext : IWorkContextStateProvider {
+        private readonly ICurrencyProvider _currencyProvider;
+
+        public CurrencyWorkContext(ICurrencyProvider currencyProvider) {
+            _currencyProvider = currencyProvider;
+        }
+
+        public Func<WorkContext, T> Get<T>(string name) {
+            if (name == "CurrentNumberFormat") {
+                return ctx => (T)(object)_currencyProvider.NumberFormat;
+            }
+            return null;
+        }
+    }
+}
