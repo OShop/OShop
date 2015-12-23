1735aeaf4ed7db33613b38a2febc8dac68a8f48f
Thierry Fleury <thierry@codinlab.fr>
2015-12-23 14:31:43 +0100

    Remove unused annotations

--- Handlers/OShopSettingsPartHandler.cs
+++ Handlers/OShopSettingsPartHandler.cs
@@ -1,9 +1,7 @@
-﻿using JetBrains.Annotations;
-using Orchard.ContentManagement.Handlers;
+﻿using Orchard.ContentManagement.Handlers;
 using OShop.Models;
 
 namespace OShop.Handlers {
-    [UsedImplicitly]
     public class OShopSettingsPartHandler : ContentHandler {
         public OShopSettingsPartHandler() {
             Filters.Add(new ActivatingFilter<OShopSettingsPart>("Site"));
--- Handlers/VatSettingsPartHandler.cs
+++ Handlers/VatSettingsPartHandler.cs
@@ -1,10 +1,8 @@
-﻿using JetBrains.Annotations;
-using Orchard.ContentManagement.Handlers;
+﻿using Orchard.ContentManagement.Handlers;
 using Orchard.Environment.Extensions;
 using OShop.Models;
 
 namespace OShop.Handlers {
-    [UsedImplicitly]
     [OrchardFeature("OShop.VAT")]
     public class VatSettingsPartHandler : ContentHandler {
         public VatSettingsPartHandler() {
