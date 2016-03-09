a0b9a83bd1c7bbec9cefeeb862d6a5acdffda330
Thierry Fleury <thierry@codinlab.fr>
2016-03-09 10:07:27 +0100

    DefaultCurrencyProvider : Apply curency number format to numbers

--- Services/DefaultCurrencyProvider.cs
+++ Services/DefaultCurrencyProvider.cs
@@ -3,11 +3,34 @@ using System;
 using System.Globalization;
 using Orchard.ContentManagement;
 using OShop.Models;
+using System.Collections.Generic;
 
 namespace OShop.Services {
     public class DefaultCurrencyProvider : ICurrencyProvider {
         private OShopSettingsPart _settings;
         private NumberFormatInfo _numberFormat;
+
+        // Converts currency negative pattern to matching number negative pattern
+        // https://msdn.microsoft.com/fr-fr/library/system.globalization.numberformatinfo.currencynegativepattern(v=vs.100).aspx
+        // https://msdn.microsoft.com/fr-fr/library/system.globalization.numberformatinfo.numbernegativepattern(v=vs.100).aspx
+        private static readonly Dictionary<int, int> NegativePatternMatching = new Dictionary<int, int>() {
+            {0, 0},
+            {1, 1},
+            {2, 1},
+            {3, 3},
+            {4, 0},
+            {5, 1},
+            {6, 3},
+            {7, 3},
+            {8, 1},
+            {9, 2},
+            {10, 4},
+            {11, 3},
+            {12, 1},
+            {13, 3},
+            {14, 0},
+            {15, 0}
+        };
 
         private OShopSettingsPart Settings {
             get {
@@ -45,10 +68,15 @@ namespace OShop.Services {
             var numberFormat = new NumberFormatInfo();
             numberFormat.CurrencySymbol = CurrencySettings.CurrencySymbol;
             numberFormat.CurrencyDecimalDigits = CurrencySettings.CurrencyDecimalDigits;
+            numberFormat.NumberDecimalDigits = CurrencySettings.CurrencyDecimalDigits;
             numberFormat.CurrencyDecimalSeparator = CurrencySettings.CurrencyDecimalSeparator;
+            numberFormat.NumberDecimalSeparator = CurrencySettings.CurrencyDecimalSeparator;
             numberFormat.CurrencyGroupSeparator = CurrencySettings.CurrencyGroupSeparator;
+            numberFormat.NumberGroupSeparator = CurrencySettings.CurrencyGroupSeparator;
             numberFormat.CurrencyGroupSizes = CurrencySettings.CurrencyGroupSizes;
+            numberFormat.NumberGroupSizes = CurrencySettings.CurrencyGroupSizes;
             numberFormat.CurrencyNegativePattern = CurrencySettings.CurrencyNegativePattern;
+            numberFormat.NumberNegativePattern = NegativePatternMatching[CurrencySettings.CurrencyNegativePattern];
             numberFormat.CurrencyPositivePattern = CurrencySettings.CurrencyPositivePattern;
 
             return numberFormat;
